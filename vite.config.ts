3d06b17a5b27304bcbbe009582bb3d1b6cfdaaba
Aarnav Tale <aarnav@tale.me>
2021-12-15 22:39:00 -0500

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
+
+export default defineConfig({
+	plugins: [reactRefresh()],
+	esbuild: {
+		jsxInject: `import React from 'react'`
+	},
+	resolve: {
+		alias: {
+			components: resolve(__dirname, 'src/components'),
+			styles: resolve(__dirname, 'src/styles'),
+			routes: resolve(__dirname, 'src/routes')
+		}
+	},
+})
